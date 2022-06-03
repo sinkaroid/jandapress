8a5f3d8fcf017a7afa93b42571cb6088887c9d8d
sinkaroid <bibsindra@gmail.com>
2022-06-03 09:56:39 +0700

    fix(options): proper export

--- utils/options.ts
+++ utils/options.ts
@@ -4,6 +4,6 @@ export default {
   HENTAI2READ: "https://hentai2read.com",
   SIMPLY_HENTAI: "https://simply-hentai.com",
   SIMPLY_HENTAI_PROXIFIED: "https://simplyh.sinxdr.workers.dev",
-  "NHENTAI": "https://nhentai.net",
-  "NHENTAI_IP": "http://35.186.156.165"
+  NHENTAI: "https://nhentai.net",
+  NHENTAI_IP: "http://35.186.156.165"
 };
