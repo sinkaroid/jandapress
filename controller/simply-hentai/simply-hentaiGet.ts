cdf564838bf03cd38d7c3965004875bb94f96d4f
sinkaroid <bibsindra@gmail.com>
2022-06-03 09:47:21 +0700

    fix(controller): remove logs and minor comments

--- controller/simply-hentai/simply-hentaiGet.ts
+++ controller/simply-hentai/simply-hentaiGet.ts
@@ -1,11 +1,9 @@
 import { scrapeContent } from "../../scraper/simply-hentai/simply-hentaiGetController";
 import c from "../../utils/options";
-import { logger } from "../../utils/logger";
 
 export async function getSimplyhentai(req: any, res: any) {
   try {
     const book = req.query.book || "";
-    //if book not following format "abc/1", trhow error
     // if (book.split("/").length !== 2) throw Error("Book must be in format 'book_example/chapter'. Example: 'fate_lewd_summoning/1'");
     const url = `${c.SIMPLY_HENTAI_PROXIFIED}/${book}`;
     console.log(url);
