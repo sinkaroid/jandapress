9c0969a83d25d7587bd4aa85f0107564e23faeb0
sinkaroid <bibsindra@gmail.com>
2022-06-04 14:06:00 +0700

    test: add response status checker

--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,8 @@ import c from "../src/utils/options";
 import p from "phin";
 
 for (const url of 
-  [c.HENTAIFOX, c.PURURIN, c.HENTAI2READ, c.SIMPLY_HENTAI, c.SIMPLY_HENTAI_PROXIFIED, c.NHENTAI, c.NHENTAI_IP]) {
+  [c.HENTAIFOX, c.PURURIN, c.HENTAI2READ, c.SIMPLY_HENTAI, 
+    c.SIMPLY_HENTAI_PROXIFIED, c.NHENTAI, c.NHENTAI_IP, c.NHENTAI_IP_2]) {
   p({ url }).then(res => {
     if (res.statusCode !== 200) {
       console.log(`${url} is not available, status code: ${res.statusCode}`);
