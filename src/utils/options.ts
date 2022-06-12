557721cb7cbb614cc08731f2e8cbe141e44e61a1
sinkaroid <bibsindra@gmail.com>
2022-06-12 07:45:31 +0700

    feat(options): add asmhentai support

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -4,6 +4,7 @@ export default {
   HENTAI2READ: "https://hentai2read.com",
   SIMPLY_HENTAI: "https://simply-hentai.com",
   SIMPLY_HENTAI_PROXIFIED: "https://simplyh.sinxdr.workers.dev",
+  ASMHENTAI: "https://asmhentai.com",
   NHENTAI: "https://nhentai.net",
   NHENTAI_IP: "http://35.186.156.165",
   NHENTAI_IP_2: "http://173.82.30.99:3002",
