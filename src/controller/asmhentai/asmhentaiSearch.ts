b24199847e5de55dee1ac2477ba22b8679ffca00
sinkaroid <bibsindra@gmail.com>
2022-06-12 07:45:05 +0700

    feat(controller): add asmhentai request

---
+++ src/controller/asmhentai/asmhentaiSearch.ts
@@ -0,0 +1,26 @@
+import { scrapeContent } from "../../scraper/asmhentai/asmHentaiSearchController";
+import c from "../../utils/options";
+import { logger } from "../../utils/logger";
+
+
+export async function searchAsmhentai(req: any, res: any, next: any) {
+  try {
+    const key = req.query.key || "";
+    const page = req.query.page || 1;
+
+    if (!key) throw Error("Parameter key is required");
+
+    const url = `${c.ASMHENTAI}/search/?q=${key}&page=${page}`;
+    const data = await scrapeContent(url);
+    logger.info({
+      path: req.path,
+      query: req.query,
+      method: req.method,
+      ip: req.ip,
+      useragent: req.get("User-Agent")
+    });
+    return res.json(data);
+  } catch (err: any) {
+    next(Error(err.message));
+  }
+}
