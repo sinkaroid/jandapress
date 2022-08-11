118aa4b1aa8dfa3446dcd80fcf586971e34ed64c
sinkaroid <bibsindra@gmail.com>
2022-08-12 02:26:08 +0700

    feat: add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,36 @@
+export interface Nhentai {
+    id: number;
+    media_id: string;
+    title: Title;
+    images: { pages: P[]}
+    scanlator: string | "";
+    upload_date: number;
+    tags: T[];
+    num_pages: number;
+    num_favorites: number;
+}
+
+export interface NhentaiSearch {
+    result: Nhentai[];
+}
+
+interface Title {
+    english: string;
+    japanese: string;
+    pretty: string;
+}
+
+interface P {
+    t: string;
+    w: number;
+    h: number;
+}
+
+interface T {
+    id: number;
+    type: string;
+    name: string;
+    url: string;
+    count: number;
+}
+
