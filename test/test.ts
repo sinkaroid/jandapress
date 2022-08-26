2b5d6badd4ea03c8ec1d39ab873772b22c5d5187
sinkaroid <bibsindra@gmail.com>
2022-08-26 20:23:02 +0700

    ci: proper status checks

--- test/test.ts
+++ test/test.ts
@@ -3,7 +3,7 @@ import p from "phin";
 
 for (const url of 
   [c.HENTAIFOX, c.PURURIN, c.HENTAI2READ, c.SIMPLY_HENTAI, 
-    c.SIMPLY_HENTAI_PROXIFIED, c.NHENTAI, c.NHENTAI_IP, c.NHENTAI_IP_2, c.ASMHENTAI]) {
+    c.SIMPLY_HENTAI_PROXIFIED, c.NHENTAI, c.NHENTAI_IP_3, c.ASMHENTAI]) {
   p({ url }).then(res => {
     if (res.statusCode !== 200) {
       console.log(`${url} is not available, status code: ${res.statusCode}`);
