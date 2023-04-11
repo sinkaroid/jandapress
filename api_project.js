00643ed3caddce342798c0cad475f9035347b56c
sinkaroid <bibsindra@gmail.com>
2023-04-11 15:17:22 +0000

    Deploying to gh-pages from @ sinkaroid/jandapress@154bb8e1993e811629d3d58106d6738c5d0848a5 🚀

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2023-04-11T15:13:17.404Z",
+    "time": "2023-04-11T15:17:19.926Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
