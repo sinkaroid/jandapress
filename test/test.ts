6fe77c345f50281b8f401834d38e1a47800a159f
sinkaroid <bibsindra@gmail.com>
2023-05-15 16:11:40 +0700

    improve tests

--- test/test.ts
+++ test/test.ts
@@ -1,15 +1,32 @@
 import c from "../src/utils/options";
 import p from "phin";
+import { name, version } from "../package.json";
 
-for (const url of 
-  [c.HENTAIFOX, c.PURURIN, c.HENTAI2READ, c.SIMPLY_HENTAI, 
-    c.SIMPLY_HENTAI_PROXIFIED, c.NHENTAI, c.NHENTAI_IP_3, c.ASMHENTAI]) {
-  p({ url }).then(res => {
-    if (res.statusCode !== 200) {
-      console.log(`${url} is not available, status code: ${res.statusCode}`);
+function getKeyByValue(data: any, value: string) {
+  return Object.keys(data).find(key => data[key] === value);
+}
+
+for (const url of
+  [
+    c.NHENTAI_IP_4,
+    c.HENTAIFOX,
+    c.PURURIN,
+    c.ASMHENTAI,
+    c.SIMPLY_HENTAI_PROXIFIED,
+    c.HENTAI2READ,
+    c.THREEHENTAI
+  ]) {
+  p({ 
+    url: url, 
+    headers: {
+      "User-Agent": `${name}/${version} Node.js/16.9.1`,
+    }
+  }).then(res => {
+    if (res.statusCode !== 200 && res.statusCode !== 308 && res.statusCode !== 301) {
+      throw new Error(`${url} of ${getKeyByValue(c, url)} is not available, status: ${res.statusCode}, couldn't be scrape`);
     }
     else {
-      console.log(`${url} is available, can be scraped`);
+      console.log(`${url} is available status: ${res.statusCode}, could be scrape`);
     }
   });
 }
