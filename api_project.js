7bda288a9df1fe5a47bb21ade04ed95d1bc110b3
sinkaroid <sinkaroid@users.noreply.github.com>
2023-05-16 08:02:25 +0000

    Deploying to gh-pages from @ sinkaroid/jandapress@250595b2254a786900b890a6c900e42bc78d4738 🚀

--- api_project.js
+++ api_project.js
@@ -3,13 +3,13 @@ define({
   "url": "https://janda.sinkaroid.org",
   "sampleUrl": "https://janda.sinkaroid.org",
   "name": "Jandapress",
-  "version": "2.1.7-alpha",
+  "version": "3.8.0-alpha",
   "description": "RESTful and experimental API for the Doujinshi, Pressing the whole nhentai, pururin, hentaifox, and more.. where the official one is lack.",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2023-05-16T07:59:34.623Z",
+    "time": "2023-05-16T08:02:24.492Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
